8c92b936b00315762fb92ba0ee73479b0322854a
Shukri Adams <shukri.adams@gmail.com>
2020-06-10 15:19:24 +0200

    fixed tests

--- src/tests/tests/server/routes/lastfm/delete.js
+++ src/tests/tests/server/routes/lastfm/delete.js
@@ -1,27 +1,26 @@
-/*
-
 const 
     assert = require('madscience-node-assert'),
     route = require(_$+'routes/lastfm'),
+    requireMock = require(_$t+'helpers/require'),
     RouteTester = require(_$t+'helpers/routeTester'),
     mocha = require(_$t+'helpers/testbase');
 
 mocha('route/lastfm/delete', async(testArgs)=>{
-
     
     it('happy path : route removes lastfm integration from profile and returns updated user content', async () => {
-        
-        let routeTester = await new RouteTester(route);
 
-        routeTester.authenticate();        
-        routeTester.setUserContent({ someUserContent : 'left hand path'});
+        // prevent delete from cascading to db
+        const profileLogic = require(_$+'logic/profiles')
+        profileLogic.removeLastfm =()=>{ } // do nothing
+        requireMock.add(_$+'logic/profiles', profileLogic)
 
-        routeTester.route.profileLogic.removeLastfm =()=>{ } // do nothing
+        let routeTester = await new RouteTester(route)
 
-        await routeTester.get('/v1/lastfm/delete');
-        assert.equal(routeTester.res.content.payload.someUserContent, 'left hand path');
-    });
+        routeTester.authenticate()
+        routeTester.setUserContent({ someUserContent : 'left hand path'})
+
+        await routeTester.get('/v1/lastfm/delete')
+        assert.equal(routeTester.res.content.payload.someUserContent, 'left hand path')
+    })
 
 })
-
-*/
--- src/tests/tests/server/routes/lastfm/scrobble.js
+++ src/tests/tests/server/routes/lastfm/scrobble.js
@@ -1,43 +1,44 @@
-/*
-
 const 
     assert = require('madscience-node-assert'),
     route = require(_$+'routes/lastfm'),
     RouteTester = require(_$t+'helpers/routeTester'),
+    requireMock = require(_$t+'helpers/require'),
     mocha = require(_$t+'helpers/testbase');
 
 mocha('route/lastfm/scrobble', async(testArgs)=>{
 
     
     it('happy path : scrobbles a play', async () => {
-        
-        let routeTester = await new RouteTester(route);
+
+        // prevent scrobble from cascading to db
+        const songsLogic = require(_$+'logic/songs')
+        songsLogic.scrobble =(profileId, song, duration)=>{ 
+            actualSong = song
+            actualDuration = duration
+            actualProfileId = profileId
+        }        
+        requireMock.add(_$+'logic/songs', songsLogic)
+
+
+        let routeTester = await new RouteTester(route)
         // user must be logged in to scrobble
-        routeTester.authenticate();     
+        routeTester.authenticate()
         // this is the song data we expect to be scrobbled (passed by query string)
-        routeTester.req.query.song = 'song1234';
-        routeTester.req.query.songDuration = 'duration1234';
+        routeTester.req.query.song = 'song1234'
+        routeTester.req.query.songDuration = 'duration1234'
 
         // intercept the actual song data scrobbled
         let actualSong = null,
             actualProfileId = null,
-            actualDuration = null;
+            actualDuration = null
 
-        routeTester.route.songsLogic.scrobble =(profileId, song, duration)=>{ 
-            actualSong = song;
-            actualDuration = duration;
-            actualProfileId = profileId;
-        }
+        await routeTester.get('/v1/lastfm/scrobble')
 
-        await routeTester.get('/v1/lastfm/scrobble');
-
-        assert.equal(actualSong, 'song1234');
-        assert.equal(actualDuration, 'duration1234');
-        assert.equal(actualProfileId, routeTester.authToken.profileId);
+        assert.equal(actualSong, 'song1234')
+        assert.equal(actualDuration, 'duration1234')
+        assert.equal(actualProfileId, routeTester.authToken.profileId)
         // the payload for a scrobble is an empty object, confirm by ensuring it's an object with no properties
-        assert.empty(Object.keys(routeTester.res.content.payload));
+        assert.empty(Object.keys(routeTester.res.content.payload))
     });
 
 })
-
-*/
