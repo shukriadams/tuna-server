3cf18e625b7519cc686b0ce628c633cbe3e47216
Shukri Adams <shukri.adams@gmail.com>
2020-06-11 01:21:47 +0200

    fixed test

--- src/tests/tests/server/routes/oauth/dropbox.js
+++ src/tests/tests/server/routes/oauth/dropbox.js
@@ -1,51 +1,57 @@
-/*
-
 const 
     constants = require(_$+'types/constants'),
     assert = require('madscience-node-assert'),
     route = require(_$+'routes/oauth'),
     RouteTester = require(_$t+'helpers/routeTester'),
-    mocha = require(_$t+'helpers/testbase');
+    mocha = require(_$t+'helpers/testbase')
 
 mocha('route/oauth/dropbox', async(testArgs)=>{
     
     it('route/oauth/dropbox : happy path : swap succeeds and redirects to next page', async () => {
         
-        let routeTester = await new RouteTester(route);
-        // code to be converted to token
-        routeTester.req.query.code = 'some-code';
-        // combination of user's authtoken + page to redirect to
-        routeTester.req.query.state = 'the-auth-token_the-page';
+        const authTokenLogic = require(_$+'logic/authToken')
+        authTokenLogic.getById =()=>{ return { profileId: 'a-profile' }}
+        requireMock.add(_$+'logic/authToken', authTokenLogic)
 
-        routeTester.route.authTokenLogic.getById =()=>{ return { profileId: 'a-profile' }}
+        const dropboxHelper = require(_$+'helpers/dropbox/common')
+        dropboxHelper.swapCodeForToken =(profileId, code)=>{ 
+            actualProfileId = profileId
+            actualCode = code
+        }
+        requireMock.add(_$+'helpers/dropbox/common', dropboxHelper)
+
+        
+        let routeTester = await new RouteTester(route)
+        // code to be converted to token
+        routeTester.req.query.code = 'some-code'
+        // combination of user's authtoken + page to redirect to
+        routeTester.req.query.state = 'the-auth-token_the-page'
+
 
         // capture actual used profile and code
         let actualProfileId,
-            actualCode;
+            actualCode
 
-        routeTester.route.dropboxHelper.swapCodeForToken =(profileId, code)=>{ 
-            actualProfileId = profileId;
-            actualCode = code;
-        }
 
-        await routeTester.get('/v1/oauth/dropbox');
 
-        assert.equal(actualCode, 'some-code');
-        assert.equal(actualProfileId, 'a-profile' );
-        assert.equal(routeTester.res.redirected, '/the-page');
-    });
+        await routeTester.get('/v1/oauth/dropbox')
+
+        assert.equal(actualCode, 'some-code')
+        assert.equal(actualProfileId, 'a-profile' )
+        assert.equal(routeTester.res.redirected, '/the-page')
+    })
+
 
     it('route/oauth/dropbox : unhappy path : throws auth error authtoken invalid', async () => {
-        
-        let routeTester = await new RouteTester(route);
+
+        let routeTester = await new RouteTester(route)
         // ensure authtoken is null
         routeTester.route.authTokenLogic.getById =()=>{ return null }
 
-        await routeTester.get('/v1/oauth/dropbox');
+        await routeTester.get('/v1/oauth/dropbox')
 
-        assert.equal(routeTester.res.content.code, constants.ERROR_INVALID_USER_OR_SESSION);
-    });
+        assert.equal(routeTester.res.content.code, constants.ERROR_INVALID_USER_OR_SESSION)
+    })
 
 })
 
-*/
