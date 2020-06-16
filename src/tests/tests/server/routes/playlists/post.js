8456bffc012c18c2a8f022aa194670dc157a5262
Shukri Adams <shukri.adams@gmail.com>
2020-06-16 23:17:16 +0200

    more test fixes

--- src/tests/tests/server/routes/playlists/post.js
+++ src/tests/tests/server/routes/playlists/post.js
@@ -1,30 +1,32 @@
-/*
+
 const 
     assert = require('madscience-node-assert'),
-    route = require(_$+'routes/playlists'),
     RouteTester = require(_$t+'helpers/routeTester'),
+    inject = require(_$t+'helpers/inject'),
     mocha = require(_$t+'helpers/testbase')
 
 mocha('route/playlists/post', async(testArgs)=>{
     
     it('route/playlists/post : happy path : creates a playlist, returns user content', async () => {
-        
-        let routeTester = await new RouteTester(route)
-        routeTester.authenticate()
-        // this is our playlist content
-        routeTester.req.body = { foo : 'bar' }
-
         // read back actual values sent to playlist create
         let actualPlaylist,
-            actualProfileId
+            actualProfileId,
+            route = require(_$+'routes/playlists'),
+            routeTester = await new RouteTester(route)
 
-        routeTester.route.playlistLogic.create = (playlist, profileId )=>{
-            actualPlaylist = playlist
-            actualProfileId = profileId
-        }
-
+        inject.object(_$+'logic/playlists', {
+            create : (playlist, profileId )=>{
+                actualPlaylist = playlist
+                actualProfileId = profileId
+            }
+        })
+        
         // return some user content
-        routeTester.route.profileLogic.buildUserContent = ()=>{ return { someUserContent : 'override the overture' } }
+        routeTester.setUserContent({ someUserContent : 'override the overture' })
+        routeTester.authenticate()
+        
+        // this is our playlist content
+        routeTester.req.body = { foo : 'bar' }
 
         await routeTester.post('/v1/playlists')
 
@@ -35,21 +37,24 @@ mocha('route/playlists/post', async(testArgs)=>{
 
     
     it('route/playlists/post : happy path : updates a playlist, returns user content', async () => {
-        
-        let routeTester = await new RouteTester(route)
-        routeTester.authenticate()
-        // this is our playlist content. setting an id will switch from create to update
-        routeTester.req.body = { bar : 'foo', id : 123 }
 
         // read back actual values sent to playlist create
-        let actualPlaylist
+        let actualPlaylist,
+            route = require(_$+'routes/playlists'),
+            routeTester = await new RouteTester(route)
 
-        routeTester.route.playlistLogic.update = (playlist)=>{
-            actualPlaylist = playlist
-        }
+        inject.object(_$+'logic/playlists', {
+            update : (playlist)=>{
+                actualPlaylist = playlist
+            }
+        })
 
         // return some user content
-        routeTester.route.profileLogic.buildUserContent = ()=>{ return { someUserContent : 'soulless' } }
+        routeTester.setUserContent({ someUserContent : 'soulless' })
+        routeTester.authenticate()
+
+        // this is our playlist content. setting an id will switch from create to update
+        routeTester.req.body = { bar : 'foo', id : 123 }
 
         await routeTester.post('/v1/playlists')
 
@@ -59,4 +64,3 @@ mocha('route/playlists/post', async(testArgs)=>{
     
 })
 
-*/
--- src/tests/tests/server/routes/profile/delete.js
+++ src/tests/tests/server/routes/profile/delete.js
@@ -1,35 +1,35 @@
-/*
-
 const 
     assert = require('madscience-node-assert'),
     route = require(_$+'routes/profile'),
     RouteTester = require(_$t+'helpers/routeTester'),
-    mocha = require(_$t+'helpers/testbase');
+    inject = require(_$t+'helpers/inject'),
+    mocha = require(_$t+'helpers/testbase')
 
 mocha('route/profiles/delete', async(testArgs)=>{
     
     it('route/profiles/delete : happy path : updates a profile, returns user content', async () => {
         
-        let routeTester = await new RouteTester(route);
-        routeTester.authenticate();
-        routeTester.req.query.key = 1234;
+        let routeTester = await new RouteTester(route),
+            actualProfileId,
+            actualKey
+
+        routeTester.authenticate()
+        routeTester.req.query.key = 1234
 
         // read back actual values sent to playlist create
-        let actualProfileId,
-            actualKey;
-        
-        routeTester.route.profileLogic.processDeleteAccount = (profileId, key)=>{
-            actualProfileId = profileId;
-            actualKey = key;
-        }
 
-        await routeTester.get('/v1/profile/delete');
+        inject.object(_$+'logic/profiles', {
+            processDeleteAccount : (profileId, key) =>{
+                actualProfileId = profileId
+                actualKey = key
+            }
+        })
 
-        assert.equal(actualKey, 1234);
-        assert.equal(actualProfileId, routeTester.authToken.profileId );
-        assert.equal(routeTester.res.content.code, 0 );
-    });
+        await routeTester.get('/v1/profile/delete')
+
+        assert.equal(actualKey, 1234)
+        assert.equal(actualProfileId, routeTester.authToken.profileId )
+        assert.null(routeTester.res.content.code) // aka empty response
+    })
     
 })
-
-*/
