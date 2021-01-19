a1df91a732d032cea0a525bcfa7f66f7a86aa2bb
Shukri Adams <shukri.adams@gmail.com>
2021-01-19 23:21:37 +0100

    added test

--- src/tests/local/test/server/sources/nextcloud/helper/ensureIntegration.js
+++ src/tests/local/test/server/sources/nextcloud/helper/ensureIntegration.js
@@ -1,10 +1,23 @@
 describe('sources/nextcloud/helper/ensureIntegration', ()=>{
 
-    /**
-     * 
-     */
-    it('sources/nextcloud/helper/ensureIntegration::happy::ensures integration', async () => {
+    it('sources/nextcloud/helper/ensureIntegration::happy::ensures integration token expired', async () => {
+        const ctx = require(_$t+'testcontext'),
+            constants = require(_$+'types/constants')
 
+        ctx.inject.object(_$+'logic/profiles', {
+            getById (){
+                let sources = {}
+                sources[constants.SOURCES_NEXTCLOUD] = {
+                    tokenDate : '2000-1-1',
+                    expiresIn : 0
+                }
+
+                return {
+                    sources
+                }
+            }
+
+        })
     })
 
 })
